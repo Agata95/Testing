f4843dbde93c01f22acb2891193eb01a112db5c2
Agata <agata.weltrowska@outlook.com>
2019-07-04 20:35:20 +0200

    testy

---
+++ src/test/java/com/javagda25/CalcularotTests.java
@@ -0,0 +1,14 @@
+package com.javagda25;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+
+public class CalcularotTests {
+    @Test
+    public void calculatorShouldReturn10WhenAdding5And5() {
+
+    }
+}
